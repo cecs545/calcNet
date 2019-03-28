c63d7ce946e5ef4463f87efd82df90ddc3f85302
vidul <vidul.verma@outlook.com>
2019-03-28 13:59:03 -0700

    queue with 2 t machines

--- machines.js
+++ machines.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
     initiateDataSet();
     let mContainer = new Container();
     // mContainer.A('Z=5*X^2+7*X*Y+3*Y^2+1');
-    mContainer.A('Z=5*X^2+7*X*Y+3*Y^2+1 ');
-
+    mContainer.A('Z=5*X^2+7*Y^2');
+    mContainer.A('Z=5*X^2+7*Y^3');
 });
 
 
@@ -15,8 +15,8 @@ $(document).ready(function () {
 function initiateDataSet() {
     objectD = new D();
     Object.freeze(objectD);
-    objectD.setVariableBox('X', 10);
-    objectD.setVariableBox('Y', 1);
+    objectD.setVariableBox('X', 2);
+    objectD.setVariableBox('Y', 2);
     objectD.setVariableBox('Z', 1);
 }
 
@@ -32,6 +32,8 @@ class Container {
         this.oQueueE = new Queue();
         this.iQueueT = new Queue();
         this.oQueueT = new Queue();
+        this.iQueueT2 = new Queue();
+        this.oQueueT2 = new Queue();
         this.iQueueP = new Queue();
         this.oQueueP = new Queue();
         this.iQueueD = new Queue();
@@ -46,16 +48,28 @@ class Container {
     }
 
     E() {
+        alert('we are inside E');
         while (!this.iQueueE.isEmpty()) {
             console.log('E machine got the input ' + this.iQueueE.front());
 
             let terms = this.iQueueE.dequeue().split('+');
             this.ExprLength = terms.length;
             console.log("Expre len " + this.ExprLength);
+            let counter = 1;
             terms.forEach((term) => {
                 console.log("term sent is " + term)
-                this.iQueueT.enqueue(term);
-                this.T()
+                if (counter % 2 == 0) {
+                    this.iQueueT.enqueue(term);
+                    this.T()
+                } else {
+                    this.iQueueT2.enqueue(term);
+                    this.T2();
+                }
+                counter++;
+
+
+
+
             })
 
         }
@@ -78,6 +92,8 @@ class Container {
     }
 
     T() {
+        alert('we are inside T1');
+        console.log("i am in t1");
         while (!this.iQueueT.isEmpty()) {
 
             let term = this.iQueueT.dequeue();
@@ -90,8 +106,38 @@ class Container {
                 } else if (factors[i].length > 1) {
 
                     this.iQueueP.enqueue(factors[i])
-                    console.log("result is " + this.oQueueT.front())
-                    resultMul = resultMul * this.P();
+                    this.P();
+                    resultMul = resultMul * this.oQueueT.dequeue();
+                    console.log("result is " + resultMul);
+                    // resultMul = resultMul * this.P();
+                } else {
+                    resultMul = resultMul * objectD.getVariableBoxVal(factors[i]);
+                }
+            }
+            this.oQueueE.enqueue(resultMul);
+            this.E()
+        }
+    }
+
+    T2() {
+        alert('we are inside T2');
+        console.log("i am in t2");
+        while (!this.iQueueT2.isEmpty()) {
+
+            let term = this.iQueueT2.dequeue();
+            console.log("term is " + term)
+            let factors = term.split('*');
+            let resultMul = 1;
+            for (let i = 0; i < factors.length; i++) {
+                if (!isNaN(factors[i])) {
+                    resultMul = resultMul * factors[i];
+                } else if (factors[i].length > 1) {
+
+                    this.iQueueP.enqueue(factors[i])
+                    this.P();
+                    resultMul = resultMul * this.oQueueT.dequeue();
+                    console.log("result is " + resultMul);
+                    // resultMul = resultMul * this.P();
                 } else {
                     resultMul = resultMul * objectD.getVariableBoxVal(factors[i]);
                 }
@@ -102,18 +148,21 @@ class Container {
     }
 
     P() {
+        alert('we are inside P');
         while (!this.iQueueP.isEmpty()) {
             console.log('factor is ' + this.iQueueP.front())
             let numPow = this.iQueueP.dequeue().split('^');
             console.log("calc power for " + numPow[0] + " ^ " + numPow[1])
             if (isNaN(numPow[0])) {
-                // let val = Math.pow(objectD.getVariableBoxVal(numPow[0]), numPow[1]);
-                // this.oQueueT.enqueue();
-                return Math.pow(objectD.getVariableBoxVal(numPow[0]), numPow[1]);
+                let val = Math.pow(objectD.getVariableBoxVal(numPow[0]), numPow[1]);
+                this.oQueueT.enqueue(val);
+                // return Math.pow(objectD.getVariableBoxVal(numPow[0]), numPow[1]);
 
+            } else {
+                this.oQueueT.enqueue(Math.pow(numPow[0], numPow[1]));
             }
-            // this.oQueueT.enqueue(Math.pow(numPow[0], numPow[1]));
-            return Math.pow(numPow[0], numPow[1]);
+
+            // return Math.pow(numPow[0], numPow[1]);
         }
 
     }
